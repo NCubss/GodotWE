c066e285a3d4e62c0f63083d907defffeed114ca
NCubs <business.npgaming@gmail.com>
2026-03-11 19:49:11 +0200

    fix white WORLD ENGINE texture

--- scripts/ui/logo.gd
+++ scripts/ui/logo.gd
@@ -16,26 +16,26 @@ static var _is_christmas: bool = Time.get_date_dict_from_system().month == 12
 		queue_redraw()
 		update_minimum_size()
 
-var _super_white = load("uid://8vb4lqljf8pe")
-var _super_christmas_white = load("uid://cstwnone4m3ko")
-var _super_black = load("uid://dcaeq18ofu4gu")
-var _super_christmas_black = load("uid://drynii8hafvrl")
-var _m_white = load("uid://6advqv4u43bl")
-var _m_black = load("uid://lrxkrdwj64ev")
-var _a_white = load("uid://htheo8wds0hk")
-var _a_black = load("uid://bj56n00f1wuih")
-var _r_white = load("uid://dxwl5jem3d8sh")
-var _r_black = load("uid://coqaj6sfbsbwl")
-var _i_white = load("uid://bc28e1ek38n6f")
-var _i_black = load("uid://d3eu5ogdnqobn")
-var _o_white = load("uid://sxke6lkwdbh3")
-var _o_black = load("uid://bcoi5ad4r8i6u")
-var _k_white = load("uid://c8y3c16aofccm")
-var _k_black = load("uid://bnlrb708t8vp7")
-var _e_white = load("uid://4yhh82t0ofop")
-var _e_black = load("uid://bx0taogf05fpm")
-var _world_engine_white = load("uid://d6x8usyvvdni")
-var _world_engine_black = load("uid://bl7gsi2cidfvv")
+const _SUPER_WHITE = preload("uid://8vb4lqljf8pe")
+const _SUPER_CHRISTMAS_WHITE = preload("uid://cstwnone4m3ko")
+const _SUPER_BLACK = preload("uid://dcaeq18ofu4gu")
+const _SUPER_CHRISTMAS_BLACK = preload("uid://drynii8hafvrl")
+const _M_WHITE = preload("uid://6advqv4u43bl")
+const _M_BLACK = preload("uid://lrxkrdwj64ev")
+const _A_WHITE = preload("uid://htheo8wds0hk")
+const _A_BLACK = preload("uid://bj56n00f1wuih")
+const _R_WHITE = preload("uid://dxwl5jem3d8sh")
+const _R_BLACK = preload("uid://coqaj6sfbsbwl")
+const _I_WHITE = preload("uid://bc28e1ek38n6f")
+const _I_BLACK = preload("uid://d3eu5ogdnqobn")
+const _O_WHITE = preload("uid://sxke6lkwdbh3")
+const _O_BLACK = preload("uid://bcoi5ad4r8i6u")
+const _K_WHITE = preload("uid://c8y3c16aofccm")
+const _K_BLACK = preload("uid://bnlrb708t8vp7")
+const _E_WHITE = preload("uid://4yhh82t0ofop")
+const _E_BLACK = preload("uid://bx0taogf05fpm")
+const _WORLD_ENGINE_WHITE = preload("uid://d6x8usyvvdni")
+const _WORLD_ENGINE_BLACK = preload("uid://bl7gsi2cidfvv")
 
 
 func _get_minimum_size() -> Vector2:
@@ -46,8 +46,7 @@ func _get_minimum_size() -> Vector2:
 
 
 func _draw() -> void:
-	#draw_texture(SUPER_BLACK, Vector2(0, 0))
-	draw_texture_rect(_get_super(), Rect2(0, 0, 219, 78), false)
+	draw_texture(_get_super(), Vector2(0, 0))
 	draw_texture_rect(_get_m(), Rect2(21, 90, 57, 69), false)
 	draw_texture_rect(_get_a(), Rect2(90, 90, 45, 69), false)
 	draw_texture_rect(_get_r(), Rect2(147, 90, 45, 69), false)
@@ -59,73 +58,73 @@ func _draw() -> void:
 	draw_texture_rect(_get_e(), Rect2(510, 90, 39, 69), false)
 	draw_texture_rect(_get_r(), Rect2(561, 90, 45, 69), false)
 	if world_engine:
-		draw_texture_rect(_get_world_engine(), Rect2(384, 183, 222, 42), false)
+		draw_texture(_get_world_engine(), Vector2(384, 183))
 
 
 func _get_super() -> Texture2D:
 	if white:
 		if _is_christmas:
-			return _super_christmas_white
+			return _SUPER_CHRISTMAS_WHITE
 		else:
-			return _super_white
+			return _SUPER_WHITE
 	else:
 		if _is_christmas:
-			return _super_christmas_black
+			return _SUPER_CHRISTMAS_BLACK
 		else:
-			return _super_black
+			return _SUPER_BLACK
 
 
 func _get_world_engine() -> Texture2D:
 	if white:
-		return _world_engine_white
+		return _WORLD_ENGINE_WHITE
 	else:
-		return _world_engine_black
+		return _WORLD_ENGINE_BLACK
 
 
 func _get_m() -> Texture2D:
 	if white:
-		return _m_white
+		return _M_WHITE
 	else:
-		return _m_black
+		return _M_BLACK
 
 
 func _get_a() -> Texture2D:
 	if white:
-		return _a_white
+		return _A_WHITE
 	else:
-		return _a_black
+		return _A_BLACK
 
 
 func _get_r() -> Texture2D:
 	if white:
-		return _r_white
+		return _R_WHITE
 	else:
-		return _r_black
+		return _R_BLACK
 
 
 func _get_i() -> Texture2D:
 	if white:
-		return _i_white
+		return _I_WHITE
 	else:
-		return _i_black
+		return _I_BLACK
 
 
 func _get_o() -> Texture2D:
 	if white:
-		return _o_white
+		return _O_WHITE
 	else:
-		return _o_black
+		return _O_BLACK
 
 
 func _get_k() -> Texture2D:
 	if white:
-		return _k_white
+		return _K_WHITE
 	else:
-		return _k_black
+		return _K_BLACK
 
 
 func _get_e() -> Texture2D:
 	if white:
-		return _e_white
+		return _E_WHITE
 	else:
-		return _e_black
+		return _E_BLACK
