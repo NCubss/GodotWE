2225b369cbd01cbc35d6bb2969f54fa34636f3ff
Sunflower <sunflower2pea@outlook.com>
2026-05-31 17:59:20 +0800

    feat: enhance coin collection feedback
    - Coins now increment one by one per physics frame instead of all at once
    - Add configurable option to set the number of coins to collect
    
    添加特性:
    金币现在每物理帧显示数量+1，而不是一次性+n
    给金币加了个自选数量的选项

--- scripts/level/tiles/coin.gd
+++ scripts/level/tiles/coin.gd
@@ -1,10 +1,15 @@
 class_name Coin
 extends Area2D
 
+@export var coin_val: int = 1
 
 func _body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.coins += 1
+	if not is_queued_for_deletion() and body is Player:
+		EventBus.emit(
+			EventBusConstants.N_PLAYER_ADDED_COIN,
+			[body, coin_val]
+		)
+		body.coins += coin_val
 		body.get_node(^"%Coin").play()
 		var sparkle = preload("uid://b4pkpe44vveuw").instantiate()
 		sparkle.global_position = global_position
--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -6,13 +6,57 @@ extends CanvasLayer
 @export var lives_label: Label
 @export var coins_label: Label
 
+# 当前显示的金币
+var cur_coin: int = 0
+# 还要加的金币
+var need_add_coin:int = 0
+
+
 ## The level associated with this HUD.
 var level: Level
 ## The player currently being kept track of.
-var player: Player
+var player: Player:
+	set(val):
+		player = val
+		cur_coin = val.coins
+		need_add_coin = 0
+		update_coins_label()
 
 
-func _process(_delta: float) -> void:
+func _enter_tree() -> void:
+	# 我不确定这样做对不对
+	assert(
+		_on_player_add_coin.callv
+		 == _on_player_add_coin.callv
+	)
+	EventBus.subscribe(
+		EventBusConstants.N_PLAYER_ADDED_COIN,
+		_on_player_add_coin.callv
+	)
+
+func _exit_tree() -> void:
+	
+	EventBus.unsubscribe(
+		EventBusConstants.N_PLAYER_ADDED_COIN,
+		_on_player_add_coin.callv
+	)
+
+
+func _on_player_add_coin(tar: Player, coin:int):
+	if tar != player:
+		return
+	need_add_coin += coin
+
+func _physics_process(_delta: float) -> void:
 	time_label.text = "%03d" % level.get_current_time()
 	if player != null:
-		coins_label.text = "%02d" % player.coins
+		if need_add_coin > 0:
+			update_coins_label()
+
+
+func update_coins_label():
+	if need_add_coin > 0:
+		cur_coin += 1
+		need_add_coin -= 1
+	
+	coins_label.text = "%02d" % cur_coin
