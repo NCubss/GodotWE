ee1f4d7551cbee909c6bdd27db74657c20e394e7
NCubs <business.npgaming@gmail.com>
2026-08-04 00:02:51 +0300

    Add parent background scene, doc background script
    All background scenes now must inherit from background.tscn.
    Documentation added to the Background script.

--- scripts/level/background.gd
+++ scripts/level/background.gd
@@ -1,7 +1,11 @@
 class_name Background
 extends Node2D
+## Custom parallax implementation that works for any viewport size.
 
+## The texture to loop.
 @export var texture: Texture2D
+## Follow scale, e.g. if this is [code]Vector2(0.5, 0.5)[/code], then it will
+## move half as fast as the camera.
 @export var follow_scale := Vector2(1, 1)
 
 
@@ -12,10 +16,13 @@ func _process(_delta: float) -> void:
 func _draw() -> void:
 	if texture == null:
 		return
+
 	var mult = Vector2(1, 1) - follow_scale
 	draw_set_transform(mult * Utility.camera_position)
 	var rect = Utility.get_visible_rect()
 	rect.position *= follow_scale
-	var start = (rect.position / texture.get_size()).floor() * texture.get_size()
-	var end = (rect.end / texture.get_size()).ceil() * texture.get_size()
-	draw_texture_rect(texture, Rect2(start, end - start), true)
+
+	var size = texture.get_size()
+	var pos = (rect.position / size).floor() * size
+	var end = (rect.end / size).ceil() * size
+	draw_texture_rect(texture, Rect2(pos, end - pos), true)
